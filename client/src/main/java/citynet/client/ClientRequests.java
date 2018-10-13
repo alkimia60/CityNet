c09cd4e39ad6fa1a738eec9ede90fee8ca4d31c7
Javier Diaz <diazgx@diba.cat>
2018-10-13 21:04:39 +0200

    Client list, register & delete users.

--- client/src/main/java/citynet/client/ClientRequests.java
+++ client/src/main/java/citynet/client/ClientRequests.java
@@ -4,13 +4,13 @@
  */
 package citynet.client;
 
+import citynet.client.model.User;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import com.google.gson.Gson;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -23,22 +23,34 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
- class ClientRequests {
-    private static final String LOCAL_URL ="http://localhost:8084/citynet/UserManager";
-    private static final String PUBLIC_URL ="http://ec2-35-180-7-53.eu-west-3.compute.amazonaws.com:8080/citynet/UserManager";
+class ClientRequests {
+
+    private static final String LOCAL_URL = "http://localhost:8084/citynet/UserManager";
+    private static final String PUBLIC_URL = "http://ec2-35-180-7-53.eu-west-3.compute.amazonaws.com:8080/citynet/UserManager";
 
     public final static void main(String[] args) {
         ClientRequests cr = new ClientRequests();
-        cr.ListAllUsers(2,PUBLIC_URL);
+        User user = new User("alberto@usuari.com", "p455w0rd", "Alta",
+                "Usuari", "Carrer de l'usuari 9", "08581", "Mataró");
+
+        //Llistar tots els usuaris
+        //cr.listAllUsers(0, LOCAL_URL);
+        
+        //Donar d'alta un usuari com objecte user
+        cr.userRegister(user, PUBLIC_URL);
+
+        //Donar de baixa un usuari pel seu email
+        //cr.userDelete("alex@usuari.com", PUBLIC_URL);
     }
 
     /**
      * Function to request the list of all users alphabetically ordered
-     * @param screen current screen number, starting with 0 
-     * @return String with startOfTable, endOfTable and the fields of the 
+     *
+     * @param screen current screen number, starting with 0
+     * @return String with startOfTable, endOfTable and the fields of the
      * records between #. All records separated by \n
      */
-    private String ListAllUsers(int screen, String url) {
+    private String listAllUsers(int screen, String url) {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             HttpPost httpPost = new HttpPost(url);
 
@@ -71,10 +83,93 @@ import org.apache.http.util.EntityUtils;
             System.out.println("----------------------------------------");
             System.out.println(responseBody);
             return responseBody;
+
         } catch (Exception ex) {
-            Logger.getLogger(ClientRequests.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ClientRequests.class
+                    .getName()).log(Level.SEVERE, null, ex);
         }
-        return "Error ListAllUsers";
+        return "Error listAllUsers";
+    }
+
+    private String userRegister(User user, String url) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(url);
+
+            Gson gson = new Gson();
+            List<NameValuePair> nvps = new ArrayList<>();
+            nvps.add(new BasicNameValuePair("user", gson.toJson(user)));
+            nvps.add(new BasicNameValuePair("action", "UserRegister"));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+
+            System.out.println("Executing request " + httpPost.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            String responseBody = httpclient.execute(httpPost, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            return responseBody;
+
+        } catch (Exception ex) {
+            Logger.getLogger(ClientRequests.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+        return "Error userRegister";
+    }
+
+    private String userDelete(String user, String url) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(url);
+
+            List<NameValuePair> nvps = new ArrayList<>();
+            nvps.add(new BasicNameValuePair("user", user));
+            nvps.add(new BasicNameValuePair("action", "UserDelete"));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+
+            System.out.println("Executing request " + httpPost.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            String responseBody = httpclient.execute(httpPost, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            return responseBody;
+
+        } catch (Exception ex) {
+            Logger.getLogger(ClientRequests.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+        return "Error userRegister";
     }
 
 }
